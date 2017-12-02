3c335da6352571e916a3d46cdd0732318ee4c428
jwwangchn <jwwangchn@outlook.com>
2017-12-02 13:34:42 +0800

    Add Python convert the image name

--- tools/image_name_convert.m
+++ tools/image_name_convert.m
@@ -1,8 +1,8 @@
 %假设图片放置于"images\"下，需要将其规范命名到"image-convert\"下
-imgs = dir(['3dsMax render\','*.jpg']);
+imgs = dir(['H:\Data\WHU_Bottle\RAW\1_Sand\RAW\','*.jpg']);
 for i = 1:length(imgs)
-    imgPath = ['3dsMax render\',imgs(i).name];
+    imgPath = ['H:\Data\WHU_Bottle\RAW\1_Sand\RAW\',imgs(i).name];
     img = imread(imgPath);
-    imgPathTrans = ['3DSMax_Render_VOC\',num2str(i,'%06d'),'.jpg'];
+    imgPathTrans = ['H:\Data\WHU_Bottle\RAW\1_Sand\RAW\','Sand_', num2str(i,'%06d'),'.jpg'];
     imwrite(img,imgPathTrans);
 end
