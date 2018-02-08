32d8e81d2fe349a5716d683bbe69dd49821cd172
jwwangchn <jwwangchn@outlook.com>
2018-02-08 17:11:59 +0800

    add preview file

--- tools/UAV_Bottle.py
+++ tools/UAV_Bottle.py
@@ -427,6 +427,61 @@ def rename_object_name_rbbox(annotation_path, object_name = 'bottle'):
             object_name_file.text = object_name
         tree.write(annotation, xml_declaration=True)
 
+def draw_box(im, BBox, color):
+    cx, cy, h, w, angle = BBox[0:5]
+    lt = [cx - w / 2, cy - h / 2, 1]
+    rt = [cx + w / 2, cy - h / 2, 1]
+    lb = [cx - w / 2, cy + h / 2, 1]
+    rb = [cx + w / 2, cy + h / 2, 1]
+    pts = []
+    pts.append(lt)
+    pts.append(rt)
+    pts.append(rb)
+    pts.append(lb)
+    angle = -angle
+    cos_cita = np.cos(np.pi / 180 * angle)
+    sin_cita = np.sin(np.pi / 180 * angle)
+    M0 = np.array([[1, 0, 0], [0, 1, 0], [-cx, -cy, 1]])
+    M1 = np.array([[cos_cita, sin_cita, 0],
+                   [-sin_cita, cos_cita, 0], [0, 0, 1]])
+    M2 = np.array([[1, 0, 0], [0, 1, 0], [cx, cy, 1]])
+    rotation_matrix = M0.dot(M1).dot(M2)
+    rotated_pts = np.dot(np.array(pts), rotation_matrix)
+
+    cv2.line(im, (int(rotated_pts[0, 0]), int(rotated_pts[0, 1])),
+             (int(rotated_pts[1, 0]), int(rotated_pts[1, 1])), color, 5)
+    cv2.line(im, (int(rotated_pts[1, 0]), int(rotated_pts[1, 1])),
+             (int(rotated_pts[2, 0]), int(rotated_pts[2, 1])), color, 5)
+    cv2.line(im, (int(rotated_pts[2, 0]), int(rotated_pts[2, 1])),
+             (int(rotated_pts[3, 0]), int(rotated_pts[3, 1])), color, 5)
+    cv2.line(im, (int(rotated_pts[3, 0]), int(rotated_pts[3, 1])),
+             (int(rotated_pts[0, 0]), int(rotated_pts[0, 1])), color, 5)
+    return im
+
+def preview_annotated_image(root_path, image_path, bbox = 'bbox'):
+    if bbox == 'bbox':
+        annotation_path = os.path.join(root_path, 'Annotations_bbox')
+    if bbox == 'rbbox':
+        annotation_path = os.path.join(root_path, 'Annotations_rbbox')
+
+    for annotation in os.listdir(annotation_path):
+        annotation_file = os.path.join(annotation_path, annotation)
+        image_file = os.path.join(image_path, annotation.split('.')[0] + '.jpg')
+        img = cv2.imread(image_file)
+        if bbox == 'bbox':
+            structs = parse_bbox(annotation_file)
+        if bbox == 'rbbox':
+            structs = parse_rbbox(annotation_file)
+
+        for struct in structs:
+            if bbox == 'bbox':
+                [xmin, ymin, xmax, ymax] = struct['bbox']
+                img = cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 0), 3)
+            if bbox == 'rbbox':
+                img = draw_box(img, struct['bbox'], (0, 255, 0))
+        cv2.imshow('preview', img)
+        cv2.waitKey(0)
+
 if __name__ == "__main__":
     # 1. 检查图像和标注文件是否匹配
     # check_image_annotations(delete_files=False)
@@ -467,6 +522,10 @@ if __name__ == "__main__":
     # generate_train_test_val(annotation_path, save_path, 0.8, 0.8)
 
     # 9. Rename object_name of rbbox
-    annotation_path = '/home/ubuntu/data/VOCdevkit/UAV-BD/Annotations_rbbox'
-    rename_object_name_rbbox(annotation_path, object_name = 'bottle')
+    # annotation_path = '/home/ubuntu/data/VOCdevkit/UAV-BD/Annotations_rbbox'
+    # rename_object_name_rbbox(annotation_path, object_name = 'bottle')
 
+    # 10. Preview annotation file
+    root_path = '/home/ubuntu/data/VOCdevkit/UAV-BD'
+    image_path = '/home/ubuntu/data/VOCdevkit/UAV-BD/JPEGImages'
+    preview_annotated_image(root_path, image_path, 'rbbox')
